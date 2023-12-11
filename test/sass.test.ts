44d5861b72b44923065ec2995c04d32cae41068f
Robin Soo-hyuk Nam <shnam7@gmail.com>
2023-12-11 23:20:37 +0900

    fix: failure in using @use 'wdk'
    * test: add a test case for wdk/_index.scss
    
    * chore: move test files to scss directory
    
    * test: update test to run for *.test.scss or *.test.sass files only

--- test/sass.test.ts
+++ test/sass.test.ts
@@ -1,12 +1,28 @@
 import path from 'path';
 import sassTrue from 'sass-true';
 import { describe, it } from 'vitest';
+import fs from 'fs';
 
-const sassModules = ['color', 'font', 'string', 'list', 'map', 'reg'];
+const getFiles = (dir: string, recurrsive: boolean = true): string[] => {
+    let files: string[] = [];
+
+    const fileList = fs.readdirSync(dir);
+    for (const file of fileList) {
+        const name = `${dir}/${file}`;
+        if (fs.statSync(name).isDirectory()) {
+            if (recurrsive) files = files.concat(getFiles(name, recurrsive));
+        } else {
+            files.push(name);
+        }
+    }
+    return files;
+};
 
 const basePath = path.relative(process.cwd(), __dirname);
+const sassTestFiles = getFiles(path.join(basePath, 'scss'));
 
-sassModules.map((module) => {
-    const sassFile = path.join(basePath, `test-${module}.scss`);
-    sassTrue.runSass({ describe, it }, sassFile, { loadPaths: ['.'] });
-});
+sassTestFiles
+    .filter((s) => s.endsWith('.test.scss') || s.endsWith('.test.sass'))
+    .map((sassFile) => {
+        sassTrue.runSass({ describe, it }, sassFile, { loadPaths: ['.'] });
+    });
