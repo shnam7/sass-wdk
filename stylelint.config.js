2096a6d9c1f807ad418a27477461c1b048b5116a
Robin Soo-hyuk Nam <shnam7@gmail.com>
2024-02-16 08:12:14 +0900

    build:Apply stylelint.

---
+++ stylelint.config.js
@@ -0,0 +1,21 @@
+export default {
+    extends: 'stylelint-config-standard-scss',
+    rules: {
+        'at-rule-empty-line-before': null,
+        'scss/comment-no-empty': null,
+        'scss/double-slash-comment-whitespace-inside': null,
+        'scss/dollar-variable-colon-space-after': null,
+        'scss/dollar-variable-empty-line-before': null,
+        'scss/dollar-variable-pattern': null,
+        'scss/double-slash-comment-empty-line-before': null,
+        'scss/at-mixin-pattern': null,
+        'scss/at-function-pattern': null,
+        'scss/at-if-closing-brace-newline-after': null,
+        'scss/at-else-closing-brace-newline-after': null,
+        'scss/at-else-closing-brace-space-after': null,
+        'scss/at-if-closing-brace-space-after': null,
+        'scss/operator-no-unspaced': null,
+        'color-hex-length': null,
+        'value-keyword-case': null,
+    },
+}
