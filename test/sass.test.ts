b46351661a8d9cae23d7be4b9602aa71e664e923
Robin Soo-hyuk Nam <shnam7@gmail.com>
2026-01-06 08:26:50 +0900

    fix: list.remove() can handle empty inner list.
    fix: clear if-function warnings.

--- test/sass.test.ts
+++ test/sass.test.ts
@@ -1,22 +1,22 @@
-import path from 'path';
-import sassTrue from 'sass-true';
-import { describe, it } from 'vitest';
-import fs from 'fs';
+import path from 'path'
+import sassTrue from 'sass-true'
+import { describe, it } from 'vitest'
+import fs from 'fs'
 
 const getFiles = (dir: string, recurrsive: boolean = true): string[] => {
-    let files: string[] = [];
+    let files: string[] = []
 
-    const fileList = fs.readdirSync(dir);
+    const fileList = fs.readdirSync(dir)
     for (const file of fileList) {
-        const name = `${dir}/${file}`;
+        const name = `${dir}/${file}`
         if (fs.statSync(name).isDirectory()) {
-            if (recurrsive) files = files.concat(getFiles(name, recurrsive));
+            if (recurrsive) files = files.concat(getFiles(name, recurrsive))
         } else {
-            files.push(name);
+            files.push(name)
         }
     }
-    return files;
-};
+    return files
+}
 
 const basePath = path.relative(process.cwd(), __dirname)
 const sassTestFiles = getFiles(path.join(basePath, 'scss')).filter(
@@ -24,7 +24,7 @@ const sassTestFiles = getFiles(path.join(basePath, 'scss')).filter(
 )
 
 for (const sassFile of sassTestFiles) {
-    sassTrue.runSass({describe, it}, sassFile, {
-        loadPaths: ['.'],
+    sassTrue.runSass({ describe, it }, sassFile, {
+        loadPaths: ['.']
     })
 }
